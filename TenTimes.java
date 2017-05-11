6d0fb65f429dfebfbd30dd980690cedccc2035a6
Willie Suggs <klbooker09@gmail.com>
2017-05-11 12:49:27 -0500

    Add files via upload

---
+++ TenTimes.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tentimes;
+
+/**
+ *
+ * @author kendrabooker
+ */
+public class TenTimes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        for(int i = 1; i <= 10; i++){
+        
+            System.out.println(i + " Mr. Mitchell is cool.");
+        }
+    }
+    
+}
